77e421ec4c577f3676a70653925646561f0f2cbe
jeongbaul <bauljeong07@gmail.com>
2025-07-27 00:50:09 +0900

    Create join_ok.php
    회원가입 성공과 실패를 알려주는 페이지

---
+++ htdocs/pages/user/join_ok.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+  <meta charset="UTF-8">
+  <title>회원가입 완료</title>
+</head>
+<body>
+  <h2>회원가입이 완료되었습니다!</h2>
+  <a href="/">로그인 하러 가기</a>
+</body>
+</html>
