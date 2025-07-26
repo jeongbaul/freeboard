2b45c59e26a2a0a381ecb6b65f4e032794912f78
jeongbaul <bauljeong07@gmail.com>
2025-07-27 00:51:18 +0900

    Create login_ok.php
    로그인 성공, 실패 결과 확인 페이지

---
+++ htdocs/pages/user/login_ok.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['user_id'])) {
+
+    header("Location: /pages/user/login.php");
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+  <meta charset="UTF-8" />
+  <title>로그인 성공</title>
+</head>
+<body>
+  <h1>환영합니다, <?= htmlspecialchars($_SESSION['user_name']) ?>님!</h1>
+  <p>로그인에 성공했습니다.</p>
+  
+  <p><a href="/index.php">메인페이지로 이동</a></p>
+</body>
+</html>
