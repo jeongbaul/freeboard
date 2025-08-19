2a9a890781be069469d14601e16e404856404f9d
jeongbaul <bauljeong07@gmail.com>
2025-08-20 03:40:55 +0900

    Update login_ok.php

--- htdocs/pages/user/login_ok.php
+++ htdocs/pages/user/login_ok.php
@@ -1,9 +1,8 @@
 <?php
-session_start();
 
 if (!isset($_SESSION['user_id'])) {
 
-    header("Location: /pages/user/login.php");
+    header("Location: /user/login");
     exit;
 }
 ?>
@@ -18,6 +17,6 @@ if (!isset($_SESSION['user_id'])) {
   <h1>환영합니다, <?= htmlspecialchars($_SESSION['user_name']) ?>님!</h1>
   <p>로그인에 성공했습니다.</p>
   
-  <p><a href="/index.php">메인페이지로 이동</a></p>
+  <p><a href="/">메인페이지로 이동</a></p>
 </body>
 </html>
