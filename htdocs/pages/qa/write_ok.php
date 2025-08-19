ca7a26c0d7b28fc4acccd5283e317665e24e7d41
jeongbaul <bauljeong07@gmail.com>
2025-08-20 03:43:41 +0900

    Create write_ok.php

---
+++ htdocs/pages/qa/write_ok.php
@@ -0,0 +1,32 @@
+<?php
+
+include $_SERVER['DOCUMENT_ROOT'].'/lib/db.php';
+
+if(!isset($_SESSION['user_id'])){
+    die("로그인이 필요합니다.");
+}
+
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    $subject = mysqli_real_escape_string($conn, $_POST['subject']);
+    $content = mysqli_real_escape_string($conn, $_POST['content']);
+    $id      = $_SESSION['user_id'];
+    $name    = $_SESSION['user_name'];
+    $wdate   = date('Y-m-d H:i:s');
+
+    if(!$subject || !$content){
+        die("제목과 내용을 모두 입력해주세요.");
+    }
+
+    $sql = "INSERT INTO qa (name, id, subject, content, wdate) 
+            VALUES ('$name', '$id', '$subject', '$content', '$wdate')";
+
+    if(mysqli_query($conn, $sql)){
+
+        header("Location: /qa/list");
+        exit;
+    } else {
+        die("문의 저장 실패: ".mysqli_error($conn));
+    }
+} else {
+    die("잘못된 접근입니다.");
+}
